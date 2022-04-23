39115cc3b1fdd653b2427b1677f568350130cf61
psyGamer <psygamer21@gmail.com>
2022-04-23 09:37:44 +0200

    Setup Vue

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const npm_package = require('./package.json')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	css: {
+		loaderOptions: {
+			scss: {
+				additionalData: `
+				@import '@/styles/variables.scss';
+				@import '@/styles/functions.scss';
+				@import '@/styles/mixins.scss';
+				`,
+			},
+		},
+	},
 })
