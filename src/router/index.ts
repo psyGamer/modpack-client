8ac52d20d1b056797b2eb2b8dbf2081a3cb1e6a6
psyGamer <psygamer21@gmail.com>
2022-04-23 09:48:48 +0200

    Added mod list and detail mod views to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,37 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ModsView from '../views/ModsView.vue'
+import DetailedModView from '../views/DetailedModView.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView,
+	},
+	{
+		path: '/mods',
+		name: 'mods',
+		component: ModsView,
+	},
+	{
+		path: '/mod/:id',
+		name: 'mod',
+		component: DetailedModView,
+		redirect: to => ({ name: 'mod-content', params: { ...to.params, content: 'description' } }),
+		children: [
+			{
+				path: ':content(description|versions|changelog|gallery)',
+				name: 'mod-content',
+				component: DetailedModView,
+			},
+		],
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 })
 
 export default router
