adfd936e43e2f6427a9ce89695e1b58ae07214f1
psyGamer <psygamer21@gmail.com>
2022-05-06 23:50:39 +0200

    Major updates to DetailedModView

--- src/views/DetailedModView.vue
+++ src/views/DetailedModView.vue
@@ -11,24 +11,47 @@
 					<p>by {{ mod.team }}</p>
 				</div>
 			</div>
-			<icon-button src="download_white.svg" alt="Download icon" class="download-btn"
-				>Download</icon-button
-			>
+			<primary-button class="download-btn">
+				<download-icon />Add
+			</primary-button>
 		</div>
 
 		<div class="content">
-			<subroute-selector
-				class="content-selector"
-				:routes="[
-					{ route: 'description', name: 'Description' },
-					{ route: 'versions', name: 'Versions' },
-					{ route: 'changelog', name: 'Changelog' },
-					{ route: 'gallery', name: 'Gallery' },
-				]"
-				routeName="mod-content"
-				routeParamName="content"
-			/>
-			<markdown-renderer :content="mod.body" />
+			<div class="main">
+				<subroute-selector
+					class="content-selector"
+					:routes="[
+						{ route: 'description', name: 'Description' },
+						{ route: 'versions', name: 'Versions' },
+						{ route: 'changelog', name: 'Changelog' },
+						{ route: 'gallery', name: 'Gallery' },
+					]"
+					routeName="mod-content"
+					routeParamName="content"
+				/>
+				<markdown-renderer :content="mod.body" />
+			</div>
+			<div class="side">
+				<p class="description">{{ mod.description }}</p>
+				<div class="entry">
+					<div class="name"><download-icon />Downloads</div>
+					<div class="value">
+						{{ formatNumber(mod.downloads) }}
+					</div>
+				</div>
+				<div class="entry">
+					<div class="name"><refresh-icon />Updated</div>
+					<div class="value">
+						{{ formatDate(mod.updated) }}
+					</div>
+				</div>
+				<div class="entry">
+					<div class="name"><calendar-icon />Created</div>
+					<div class="value">
+						{{ formatDate(mod.published) }}
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -37,16 +60,28 @@
 import { computed, ref } from 'vue'
 import { onBeforeRouteUpdate, useRoute } from 'vue-router'
 
+import ModrinthMod from '@/types/modrinth_api/ModrinthMod'
 import fetchMod from '@/composables/fetchMod'
-import IconButton from '@/components/ui/IconButton.vue'
+import formatNumber from '@/composables/formatNumber'
+import formatDate from '@/composables/formatDate'
+import PrimaryButton from '@/components/ui/PrimaryButton.vue'
 import SubrouteSelector from '@/components/SubrouteSelector.vue'
 import MarkdownRenderer from '@/components/ui/MarkdownRenderer.vue'
-import ModrinthMod from '@/types/modrinth_api/ModrinthMod'
+import DownloadIcon from '@/components/icon/DownloadIcon.vue'
+import CalendarIcon from '@/components/icon/CalendarIcon.vue'
+import RefreshIcon from '@/components/icon/RefreshIcon.vue'
 
 type ContentType = 'description' | 'versions' | 'changelog' | 'gallery'
 
 export default {
-	components: { IconButton, MarkdownRenderer, SubrouteSelector },
+	components: {
+		PrimaryButton,
+		MarkdownRenderer,
+		SubrouteSelector,
+		DownloadIcon,
+		CalendarIcon,
+		RefreshIcon,
+	},
 	setup: () => {
 		const route = useRoute()
 
@@ -68,8 +103,8 @@ export default {
 		const imageURL = computed(() => {
 			return mod.value?.icon_url || 'https://cdn-raw.modrinth.com/placeholder.svg'
 		})
-
-		return { mod, error, imageURL, selectedContent }
+		console.log(mod)
+		return { mod, error, imageURL, selectedContent, formatNumber, formatDate, DownloadIcon }
 	},
 }
 </script>
@@ -117,13 +152,87 @@ export default {
 }
 
 .content {
-	background: $color-bg2;
-	border-radius: 2.5em;
-	margin: 2em;
+	display: flex;
+	justify-content: space-around;
 
-	.markdown {
-		max-width: 60vw;
-		margin: 0 auto;
+	.main {
+		flex: 4;
+		background: $color-bg2;
+		border-radius: 2.5em;
+		margin-block: 2em;
+		margin-inline: 2em 1em;
+
+		.content-selector {
+			margin-top: 0;
+		}
+		.markdown {
+			max-width: 60vw;
+			margin: 0 auto;
+		}
+	}
+	.side {
+		flex: 1;
+		min-width: 30rem;
+		max-width: 35rem;
+		height: fit-content;
+
+		background: $color-bg2;
+		border-radius: 2.5em;
+
+		margin-block: 2em;
+		margin-inline: 1em 2em;
+		padding: 2em;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+
+		p {
+			margin-block: 0.5em;
+		}
+
+		.description {
+			font-size: 1.6em;
+			font-weight: bold;
+			margin: 0.25em;
+		}
+
+		.entry {
+			font-size: 1.4em;
+			display: flex;
+			justify-content: space-around;
+			text-align: center;
+			width: 100%;
+			margin-top: 1.5em;
+
+			.name,
+			.value {
+				display: flex;
+				font-weight: bold;
+				align-items: flex-end;
+			}
+			.name {
+				font-size: 1.15em;
+				flex: 1;
+				text-align: start;
+				justify-content: flex-start;
+				color: $color-text-dark;
+
+				svg {
+					stroke: $color-text-dark;
+					width: 2em;
+					height: 2em;
+					margin-right: 1em;
+				}
+			}
+			.value {
+				font-size: 1.5em;
+				flex: 1;
+				text-align: end;
+				justify-content: flex-end;
+			}
+		}
 	}
 }
 </style>
