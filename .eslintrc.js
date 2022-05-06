ee591bb7d8526854639c3cc6f2c1688236687940
psyGamer <psygamer21@gmail.com>
2022-05-07 00:13:29 +0200

    Added own components for mod title

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'vue/multi-word-component-names': 'off',
+		'vue/no-unused-components': 'warn',
 	},
 }
--- src/views/DetailedModView.vue
+++ src/views/DetailedModView.vue
@@ -3,18 +3,7 @@
 	<div v-else-if="!mod">Loading...</div>
 
 	<div v-else class="mod">
-		<div class="info-bar">
-			<div class="mod-details">
-				<img :srcset="`${imageURL} 2x`" :alt="mod.title" />
-				<div class="title-and-author">
-					<h1>{{ mod.title }}</h1>
-					<p>by {{ mod.team }}</p>
-				</div>
-			</div>
-			<primary-button class="download-btn">
-				<download-icon />Add
-			</primary-button>
-		</div>
+		<mod-titlebar :mod="mod" />
 
 		<div class="content">
 			<div class="main">
@@ -57,16 +46,19 @@
 </template>
 
 <script lang="ts">
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { onBeforeRouteUpdate, useRoute } from 'vue-router'
 
 import ModrinthMod from '@/types/modrinth_api/ModrinthMod'
+
 import fetchMod from '@/composables/fetchMod'
 import formatNumber from '@/composables/formatNumber'
 import formatDate from '@/composables/formatDate'
-import PrimaryButton from '@/components/ui/PrimaryButton.vue'
+
 import SubrouteSelector from '@/components/SubrouteSelector.vue'
 import MarkdownRenderer from '@/components/ui/MarkdownRenderer.vue'
+import ModTitlebar from '@/components/mod/ModTitlebar.vue'
+
 import DownloadIcon from '@/components/icon/DownloadIcon.vue'
 import CalendarIcon from '@/components/icon/CalendarIcon.vue'
 import RefreshIcon from '@/components/icon/RefreshIcon.vue'
@@ -75,9 +67,9 @@ type ContentType = 'description' | 'versions' | 'changelog' | 'gallery'
 
 export default {
 	components: {
-		PrimaryButton,
-		MarkdownRenderer,
 		SubrouteSelector,
+		MarkdownRenderer,
+		ModTitlebar,
 		DownloadIcon,
 		CalendarIcon,
 		RefreshIcon,
@@ -100,57 +92,12 @@ export default {
 			if (to.params.content != from.params.content) reloadSelectedContent()
 		})
 
-		const imageURL = computed(() => {
-			return mod.value?.icon_url || 'https://cdn-raw.modrinth.com/placeholder.svg'
-		})
-		console.log(mod)
-		return { mod, error, imageURL, selectedContent, formatNumber, formatDate, DownloadIcon }
+		return { mod, error, selectedContent, formatNumber, formatDate, DownloadIcon }
 	},
 }
 </script>
 
 <style lang="scss">
-.info-bar {
-	font-size: 1rem;
-
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-
-	padding-inline: 2em;
-
-	.mod-details {
-		display: flex;
-		align-items: center;
-
-		font-size: 3em;
-
-		img {
-			width: 4em;
-			height: 4em;
-			border-radius: $border-radius-icon;
-		}
-		.title-and-author {
-			display: flex;
-			align-items: baseline;
-			font-weight: bold;
-
-			h1 {
-				color: $color-text-heading;
-				font-size: 1em;
-				margin-inline: 1em 0.5em;
-			}
-			p {
-				color: $color-text-subheading;
-				font-size: 0.5em;
-			}
-		}
-	}
-	.download-btn {
-		font-size: 2em;
-	}
-}
-
 .content {
 	display: flex;
 	justify-content: space-around;
