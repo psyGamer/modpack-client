64af91ba3d9d1db8ca4e3ca95cf78a37e369e898
psyGamer <psygamer21@gmail.com>
2022-06-15 16:58:59 +0200

    Fixed Tauri setup

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,36 @@
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	// Prevent vite from obscuring rust errors
+	clearScreen: true,
 	server: {
 		port: 8080,
 	},
+	// to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+	// `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+	// env variables
+	envPrefix: ['VITE_', 'TAURI_'],
+
+	build: {
+		// Tauri supports es2021
+		target: ['es2021', 'chrome97', 'safari13'],
+		// Don't minify for debug builds
+		minify: !process.env.TAURI_DEBUG && 'esbuild',
+		// Produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_DEBUG,
+	},
+
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
+
 	css: {
 		preprocessorOptions: {
 			scss: {
